93727c5cc47a124f8e6f12282aeee0f5c0ad369b
Patrick San Juan <psanjuan06.sj@gmail.com>
2018-03-06 21:02:56 -0800

    Created Task and tests

---
+++ src/main/java/com/patrick/Task/Task.java
@@ -0,0 +1,63 @@
+package com.patrick.Task;
+
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Objects;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "TASK_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    public Task(String name) {
+        this.name = name.toUpperCase();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) &&
+                Objects.equals(name, task.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name);
+    }
+}
---
+++ src/test/java/com/patrick/Task/TaskRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.patrick.Task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TaskRepositoryTest {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Test
+    public void createTask(){
+        taskRepository.save(new Task("Cleaup"));
+    }
+}
