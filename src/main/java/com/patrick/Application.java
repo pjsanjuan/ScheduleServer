35b0e4d6debfe82913caed5c3de37d7481806e30
Patrick San Juan <psanjuan06.sj@gmail.com>
2018-03-19 22:47:49 -0700

    Created integration on the User micro-service
    - Extended Spring boot's default ObjectMapper in Application.java
    - Put the code that create a JWT in TokenAuthenticationService.java into it's own function
    - Set the SessionCreatePolicy to stateless in the configure method of WebSecurityConfig

--- src/main/java/com/patrick/Application.java
+++ src/main/java/com/patrick/Application.java
@@ -1,7 +1,9 @@
 package com.patrick;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,5 +18,13 @@ public class Application {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //https://stackoverflow.com/questions/28324352/how-to-customise-the-jackson-json-mapper-implicitly-used-by-spring-boot
+    //https://stackoverflow.com/questions/39263553/how-to-customise-jackson-in-spring-boot-1-4/40051029
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer addHibernate5Module() {
+        return (jacksonObjectMapperBuilder) ->
+                jacksonObjectMapperBuilder.modulesToInstall(new Hibernate5Module());
     }
 }
